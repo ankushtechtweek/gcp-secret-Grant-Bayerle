140b5c527e7fc6c6d25e8ade997f69a01ec40382
DevOps <you@example.com>
2023-04-11 17:55:41 +0530

    dsc

--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -16,20 +16,23 @@ resource "google_secret_manager_secret_version" "secret_version" {
 */
 
 
-# Create secret manager secret and versions
+
 resource "google_secret_manager_secret" "secrets" {
   for_each = var.secrets
 
-  secret_id = each.key
+  secret_id = each.value.secret_name
 
-  replication {
-    automatic = true
-  }
-
-  dynamic "secret_version" {
-    for_each = toset([each.value])
+  dynamic "replication" {
+    for_each = each.value.replication
     content {
-      secret_data = secret_version.value
+      location = replication.value.location
     }
   }
+}
+
+resource "google_secret_manager_secret_version" "secrets" {
+  for_each = var.secrets
+
+  secret     = google_secret_manager_secret.secrets[each.key].id
+  secret_data = each.value.secret_value
 }
--- varibales.tf
+++ varibales.tf
@@ -17,20 +17,5 @@ type = any
 
 variable "secrets" {
   type = map
-  default = {
-    "OPENAI_API_KEY" = ""
-    "RECALL_API_KEY" = ""
-    "RECALL_SIGN_TOKEN" = ""
-    "SENDGRID_API_KEY" = ""
-    "TYPESENSE_SEARCH_ONLY_KEY" = ""
-    "TYPESENSE_WRITE_ONLY_KEY" = ""
-    "WEB_CLIENT_SECRET" = ""
-    "ZOOM_OAUTH_APP_CLIENT_SECRET" = ""
-    "ZOOM_OAUTH_APP_WEBHOOK_SECRET_TOKEN" = ""
-    "ZOOM_TOKEN_ENCRYPTION_KEY" = ""
-    "SECRET_12" = ""
-    "SECRET_13" = ""
-    "SECRET_14" = ""
-    "SECRET_15" = ""
-  }
+
 }
