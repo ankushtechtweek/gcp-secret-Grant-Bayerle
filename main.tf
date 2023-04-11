a3528264d5855051ae1cc2ac420857816c0acb75
DevOps <you@example.com>
2023-04-11 14:53:21 +0530

    dsc

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "secret" {
   source = "./modules/secret_manager"
 
-  project_name = var.project_name
+  #project_name = var.project_name
   #region       = var.region 
   secret       = var.secret
 }
