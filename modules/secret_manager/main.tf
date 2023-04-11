003c6605d9d2a7b7d7fcd3d3e9c84fd51b8dca0d
DevOps <you@example.com>
2023-04-11 14:59:23 +0530

    dsc

--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -3,6 +3,11 @@
 # main.tf
 resource "google_secret_manager_secret" "secret" {
   secret_id = var.secret.secret_name
+
+    replication {
+    automatic = true
+  }
+  
 }
 
 resource "google_secret_manager_secret_version" "secret_version" {
