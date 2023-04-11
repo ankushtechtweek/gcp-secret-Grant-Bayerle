5f5376c53d2c5f0ec935175bb173404d1a573269
DevOps <you@example.com>
2023-04-11 16:38:56 +0530

    dsc

--- env/dev/dev.tfvars
+++ env/dev/dev.tfvars
@@ -4,7 +4,15 @@ region       =  "us-east1"
 
 secret = {
 
-    secret_name  = "my-first-secret"
-    secret_value = "qwerty"
+OPENAI_API_KEY = "bgdfb"
+RECALL_API_KEY = "bfbfb"
+RECALL_SIGN_TOKEN = "fgbfb"
+SENDGRID_API_KEY = "fbfbg"
+TYPESENSE_SEARCH_ONLY_KEY = "fbgfbfbgfgb"
+TYPESENSE_WRITE_ONLY_KEY = "bfgbgfb"
+WEB_CLIENT_SECRET = "bfgbfgbf"
+ZOOM_OAUTH_APP_CLIENT_SECRET = "gfbfgbgfb"
+ZOOM_OAUTH_APP_WEBHOOK_SECRET_TOKEN = "gfbgfbgfb"
+ZOOM_TOKEN_ENCRYPTION_KEY = "gfbfgbfgb"
 
 }
--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -1,5 +1,5 @@
 
-
+/*
 # main.tf
 resource "google_secret_manager_secret" "secret" {
   secret_id = var.secret.secret_name
@@ -9,9 +9,21 @@ resource "google_secret_manager_secret" "secret" {
   }
 
 }
+*/
 
 resource "google_secret_manager_secret_version" "secret_version" {
   secret     = google_secret_manager_secret.secret.id
   secret_data = var.secret.secret_value
 }
 
+resource "google_secret_manager_secret" "my_secret" {
+  secret_id = "my-secret"
+
+  dynamic "secret_version" {
+    for_each = var.secret.secret
+
+    content {
+      secret_data = "${secret_version.value}"
+    }
+  }
+}
