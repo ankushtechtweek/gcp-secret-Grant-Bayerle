964276b510ba835b2b9e97b43fc421cd19d91243
DevOps <you@example.com>
2023-04-11 14:54:20 +0530

    dsc

--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -2,13 +2,13 @@
 
 # main.tf
 resource "google_secret_manager_secret" "secret" {
-  secret_id = var.secret_name
+  secret_id = var.secret.secret_name
 }
 
 resource "google_secret_manager_secret_version" "secret_version" {
   secret = google_secret_manager_secret.secret.id
   payload {
-    data = var.secret_value
+    data = var.secret.secret_value
   }
 }
 
